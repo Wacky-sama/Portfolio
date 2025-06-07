95a106a946a0c722090195c7097ee6d1f243a3af
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-06-07 08:48:47 +0800

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import LoadingScreen from "./components/LoadingScreen";
 import Navbar from "./components/NavBar";
 import HeroSection from "./components/HeroSection";
 import AboutMe from "./components/AboutMe";
 import Contact from "./components/Contact";
+import "./index.css";
+import { useState } from "react";
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
     <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <Navbar />
       <HeroSection />
       <AboutMe />
