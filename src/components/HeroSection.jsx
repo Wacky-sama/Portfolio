c7612ba74ac8548f2e25ec33836fdab47f772617
Wacky-sama <tabugadirkenjibrocks@gmail.com>
2025-09-28 13:06:15 +0800

    update favicon link and comment out job title in HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
           <div className="pt-20 flex flex-col justify-space-between h-screen bg-gradient-to-r from-purple-950 to-purple-600 text-white">
             <p className="text-3xl md:text-5xl font-bold">Hi there, I am </p>
             <h2 className="font-bold text-5xl">Kenji "Brocks" I. Tabugadir</h2>
-              <p className="mt-4 text-xl text-white">Full Stack Developer</p>
+              {/* <p className="mt-4 text-xl text-white">Full Stack Developer</p> */}
           </div>
         </section>
   )
